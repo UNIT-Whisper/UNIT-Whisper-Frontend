945695c2f9ac8fa22fee0a8c16d8066638b71a45
JIHOON LEE <jihoon7705@gmail.com>
2024-04-06 17:21:51 +0900

    feat : layout change

--- src/pages/ChatPage/index.tsx
+++ src/pages/ChatPage/index.tsx
@@ -1,20 +1,20 @@
-import { useEffect, useRef, useState } from "react";
-import Chatcloud1 from "./images/clouds1.png";
-import Chatcloud2 from "./images/clouds2.png";
-import Chatcloud3 from "./images/clouds3.png";
-import Chatcloud4 from "./images/clouds4.png";
-import Chatcloud5 from "./images/clouds5.png";
-import Chatcloud6 from "./images/clouds6.png";
-import Chatcloud7 from "./images/clouds7.png";
-import Chatcloud8 from "./images/clouds8.png";
-import Chatcloud9 from "./images/clouds9.png";
-import useCloudStore from "@/store/chat/chat";
+import { useEffect, useRef, useState } from 'react';
+import Chatcloud1 from './images/clouds1.png';
+import Chatcloud2 from './images/clouds2.png';
+import Chatcloud3 from './images/clouds3.png';
+import Chatcloud4 from './images/clouds4.png';
+import Chatcloud5 from './images/clouds5.png';
+import Chatcloud6 from './images/clouds6.png';
+import Chatcloud7 from './images/clouds7.png';
+import Chatcloud8 from './images/clouds8.png';
+import Chatcloud9 from './images/clouds9.png';
+import useCloudStore from '@/store/chat/chat';
 
 function ChatPage() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState('');
   const clouds = useCloudStore((state) => state.getClouds());
   const addCloud = useCloudStore((state) => state.addCloud);
-  const resetCloud = useCloudStore((state)=>state.resetClouds);
+  const resetCloud = useCloudStore((state) => state.resetClouds);
   const [isComposing, setIsComposing] = useState(false);
   const [leftPosition, setLeftPosition] = useState('0%');
   const [imgIdx, setImgIdx] = useState(0);
@@ -22,9 +22,9 @@ function ChatPage() {
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setData(e.target.value);
   };
-  useEffect(()=>{
+  useEffect(() => {
     resetCloud;
-  },[])
+  }, []);
   useEffect(() => {
     if (textareaRef.current) {
       textareaRef.current.style.height = 'auto'; // 높이를 auto로 설정하여 먼저 크기를 축소합니다.
@@ -37,33 +37,33 @@ function ChatPage() {
     if (isComposing) return;
     if (event.key === 'Enter') {
       event.preventDefault();
-      const newCloud =  {
-        text : data,
-        leftPosition : leftPosition,
-        randomIdx : imgIdx,
-        check : false,
-      }
+      const newCloud = {
+        text: data,
+        leftPosition: leftPosition,
+        randomIdx: imgIdx,
+        check: false,
+      };
       addCloud(newCloud);
-      setData(""); // 입력 필드 초기화
+      setData(''); // 입력 필드 초기화
     }
   };
   return (
     <>
-      <div className="absolute top-[89%] z-30 mb-[61px] w-full px-5">
+      <div className="absolute top-[91.5%] z-30 w-full max-w-[500px] px-5">
         <div className="flex w-full items-center gap-1 rounded border border-solid bg-white px-4 py-3">
           <textarea
             value={data}
             ref={textareaRef}
             onChange={onChange}
             placeholder="궁시렁 궁시렁 입력하기"
-            className="resize-none overflow-hidden"
+            className="over:outline-dashed w-full resize-none overflow-hidden outline-none"
             onKeyDown={handleKeyPress}
             onCompositionStart={() => setIsComposing(true)}
             onCompositionEnd={() => setIsComposing(false)}
           />
         </div>
       </div>
-      <div className="relative h-24 w-full">
+      <div className="relative h-24 w-full max-w-[380px]">
         {clouds.map((el, index) => (
           <Cloud key={index} data={el.randomIdx} left={el.leftPosition} index={index} />
         ))}
@@ -106,18 +106,20 @@ const Cloud = ({ data, left, index }: { data: number; left: string; index: numbe
   }, []);
 
   return (
-    <img
-      src={randomChatCloudImage}
-      alt="구름애들"
-      style={{
-        position: 'absolute',
-        left: left,
-        whiteSpace: 'pre-wrap',
-        wordWrap: 'break-word',
-        top: `${height}px`,
-        transition: 'top 1.2s ease-out',
-      }}
-    />
+    <div className="max-w-[380px]">
+      <img
+        src={randomChatCloudImage}
+        alt="구름애들"
+        style={{
+          position: 'absolute',
+          left: left,
+          whiteSpace: 'pre-wrap',
+          wordWrap: 'break-word',
+          top: `${height}px`,
+          transition: 'top 1.2s ease-out',
+        }}
+      />
+    </div>
   );
 };
 
