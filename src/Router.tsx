256ea61df129018e866ed6c24aa66d0f99a16852
JIHOON LEE <jihoon7705@gmail.com>
2024-04-06 10:33:18 +0900

    feat : Add page configuration

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,20 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import { createBrowserRouter } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ChatPage from './pages/ChatPage';
+import ChatCollectionPage from './pages/ChatCollectionPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/chat',
+    element: <ChatPage />,
+  },
+  {
+    path: '/chatCollection',
+    element: <ChatCollectionPage />,
   },
 ]);
 
